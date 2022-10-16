c6107a619acaa9630e002d4bd1dd8a861aa88821
YunKukPark <a01043340999@gmail.com>
2022-10-17 01:08:46 +0900

    Migration rest to graphql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server';
+import fetch from 'node-fetch';
 
 let tweets = [
   {
@@ -27,33 +28,74 @@ let users = [
 ];
 
 const typeDefs = gql`
+  type Query {
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+
   type User {
     id: ID!
     firstName: String!
     lastName: String!
+    """
+    Is sum of fistName and lastName
+    """
     fullName: String!
   }
-
+  """
+  A tweet is a message posted on twitter
+  """
   type Tweet {
     id: ID!
     text: String
     author: User
   }
 
-  type Query {
-    allUsers: [User!]!
-    allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet
-  }
-
   type Mutation {
     postTweet(text: String, userId: ID!): Tweet
     deleteTweet(id: ID): Boolean
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
 const resolvers = {
   Query: {
+    allMovies() {
+      return fetch('https://yts.mx/api/v2/list_movies.json')
+        .then(res => res.json())
+        .then(json => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then(res => res.json())
+        .then(json => json.data.movie);
+    },
     allUsers: () => users,
     allTweets: () => tweets,
     tweet(root, { id }) {
